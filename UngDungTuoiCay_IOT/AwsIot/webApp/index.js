5600ee480712cdc04f73a02be7109c80088c52c2
NguyenDat99 <donguyen5548@gmail.com>
2019-05-23 21:17:24 +0700

    Viet Web

---
+++ UngDungTuoiCay_IOT/AwsIot/webApp/index.js
@@ -0,0 +1,25 @@
+var awsIot = require('aws-iot-device-sdk');
+
+
+var device = awsIot.device({
+   keyPath: 'f79b16571c-private.pem.key',
+  certPath: 'f79b16571c-certificate.pem.crt',
+    caPath: 'AmazonRootCA1.pem',
+  clientId: 'MyConnect',
+   host: 'a2mbzfzdofwxgs-ats.iot.us-west-2.amazonaws.com'
+});
+
+device
+  .on('connect', function() {
+    console.log('connect');
+    device.subscribe('$aws/things/DoAnIotThing/shadow/update');
+    device.publish('$aws/things/DoAnIotThing/shadow/update', JSON.stringify({ test_data: 1}));
+  });
+
+device
+  .on('message', function(topic, payload) {
+    console.log(payload.toString());
+  });
+  app.listen(3000, function () {
+
+  })
